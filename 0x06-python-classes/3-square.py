26dd281de13f09b0490adbca44c1822625793273
stecy12 <47589787+stecy12@users.noreply.github.com>
2022-07-20 18:44:44 +0530

    Update 3-square.py
    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Creating a new class called Square"""
+"""Create a Class Square with size and method of area"""
 
 
 class Square:
-    """class Square that defines a square by: (based on 1-square.py)"""
+    """Class - Square"""
+
     def __init__(self, size=0):
-        """Initialize Square with size attribute"""
-        self.__size = size
-
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        """Constructor of a Square with the size"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
 
     def area(self):
-        """Method that returns the current square area"""
-        return self.__size * self.
+        """Method to get the area of the Square"""
+        return (self.__size ** 2)
